60695256f2553bd06a90852f5263cbd34a567437
root <rootbis@xUbuntu.myguest.virtualbox.org>
2025-12-30 21:21:24 +0100

    adding new texture

--- srcs/graphic/texture.py
+++ srcs/graphic/texture.py
@@ -8,7 +8,7 @@ def texture_init(_game):
     snake_sprite = pygame.transform.scale((snake_sprite), (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(snake_sprite)
     # -- Snake body -- #
-    snake_body_sprite = pygame.image.load("../texture/body.jpg").convert()
+    snake_body_sprite = pygame.image.load("../texture/body.png").convert()
     snake_body_sprite = pygame.transform.scale((snake_body_sprite), (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(snake_body_sprite)
     # -- Wall -- # 
@@ -24,7 +24,7 @@ def texture_init(_game):
     r_apple_sprite = pygame.transform.scale(r_apple_sprite, (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(r_apple_sprite)
     # -- G_apple -- #
-    g_apple_sprite = pygame.image.load("../texture/g_apple.jpg").convert()
+    g_apple_sprite = pygame.image.load("../texture/green_.png").convert()
     g_apple_sprite = pygame.transform.scale(g_apple_sprite, (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(g_apple_sprite)
     return (dico_texture)
